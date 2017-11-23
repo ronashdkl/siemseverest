19b63dc1ad90f509ade5284ef86fcf3a16b7b102
prem111 <premchhantyal2010@gmail.com>
2017-11-23 10:57:24 +0545

    remove unwanted js

--- assets/AdminAsset.php
+++ assets/AdminAsset.php
@@ -18,18 +18,14 @@ class AdminAsset extends AssetBundle
         'fonts/font-awesome.min.css',
         'fonts/ionicons.min.css',
         'dist/css/AdminLTE.min.css',
-        'dist/css/AdminLTE.min.csss',
         'dist/css/skins/_all-skins.min.css',
         ];
     public $js = [
+        'bootstrap/js/bootstrap.min.js',
         'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js',
         'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
-        'plugins/jQuery/jquery-2.2.3.min.js',
-        'bootstrap/js/bootstrap.min.js',
-        'plugins/slimScroll/jquery.slimscroll.min.jss',
         'plugins/fastclick/fastclick.js',
         'dist/js/app.min.js',
-        'dist/js/demo.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
