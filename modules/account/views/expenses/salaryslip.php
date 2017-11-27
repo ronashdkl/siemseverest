4d8a326c718e2af09e0078363f789d9f37f068e8
binay1234567890 <pikaju789@gmail.com>
2017-11-27 17:05:19 +0545

    salary slip basic table

--- modules/account/views/expenses/salaryslip.php
+++ modules/account/views/expenses/salaryslip.php
@@ -26,22 +26,47 @@
     </thead>
     <tbody>
     <tr>
-        <td colspan="4" class="center"><h5>SIEMS EVEREST CONSULTANCY<h2></h2></td>
-        <td colspan="4"><h2>SALARY SLIP</h2></td>
-        <td colspan="4" class="center"><h5>Changunarayan-2,Bhaktapur,Nepal</h5></td>
+        <td colspan="3" class="center"><h5>SIEMS EVEREST CONSULTANCY<h2></h2></td>
+        <td colspan="6"><h2>SALARY SLIP</h2></td>
+        <td colspan="3" class="center"><h5>Changunarayan-2,Bhaktapur,Nepal</h5></td>
     </tr>
     <tr>
-        <td colspan="5">Employee Name : </td>
-        <td colspan="7">Mukesh Pandey </td>
+        <td>Employee Name : </td>
+        <td>Mukesh Pandey </td>
     </tr>
     <tr>
-        <td colspan="5">Designation   : </td>
-        <td colspan="7">Accountant </td>
+        <td >Designation   : </td>
+        <td>Accountant </td>
     </tr>
     <tr>
-        <td>D</td>
-        <td>E</td>
-        <td>F</td>
+        <td colspan="6">Description</td>
+        <td colspan="3">Earning</td>
+        <td colspan="3">Deduction</td>
+    </tr>
+    <tr>
+        <td colspan="6">Basic Salary</td>
+        <td colspan="3">$60.39</td>
+        <td colspan="3">-</td>
+    </tr>
+    <tr>
+        <td colspan="6">Tax deduction</td>
+        <td colspan="3">-</td>
+        <td colspan="3">$0.39</td>
+    </tr>
+    <tr>
+        <td colspan="6">Total</td>
+        <td colspan="3">$60.39</td>
+        <td colspan="3">$0.39</td>
+    </tr>
+    <tr>
+        <td colspan="6" rowspan="4">Total</td>
+        <td class="center">NET PAY</td>
+    </tr>
+    <tr>
+        <td colspan="3" class="center">$61.39</td>
+    </tr>
+    <tr>
+        <td rowspan="2" class="center">SIXTY ONE DOLLAR</td>
     </tr>
     </tbody>
 </table>
