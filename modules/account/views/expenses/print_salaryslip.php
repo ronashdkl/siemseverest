f7eece89bab2bb5fbf1337277ea61c827e342b8a
binay1234567890 <pikaju789@gmail.com>
2017-11-28 14:09:55 +0545

    mpdf added for pay slip

---
+++ modules/account/views/expenses/print_salaryslip.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Binay
+ * Date: 28/11/2017
+ * Time: 13:56
+ */
+?>
+<table>
+    <tbody>
+    <tr>
+        <td style="min-width: 200px; min-height: 150px;"><h5><b>SIEMS EVEREST CONSULTANCY</b><h2></td>
+        <td ><h2>SALARY SLIP</h2></td>
+        <td><h5>Changunarayan-2,Bhaktapur,Nepal</h5></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>October 2017 </td>
+        <td ></td>
+
+    </tr>
+    <tr>
+        <td >Employee Name : </td>
+        <td >Mukesh Pandey </td>
+    </tr>
+    <tr>
+        <td >Designation   : </td>
+        <td >Accountant </td>
+    </tr>
+    <tr>
+        <td ><b>Description</b></td>
+        <td ><b>Earning</b></td>
+        <td ><b>Deduction</b></td>
+    </tr>
+    <tr>
+        <td >Basic Salary</td>
+        <td >$60.39</td>
+        <td >-</td>
+    </tr>
+    <tr>
+        <td >Tax deduction</td>
+        <td >-</td>
+        <td >$0.39</td>
+    </tr>
+    <tr>
+        <td>Total</td>
+        <td ><b>$60.39</b></td>
+        <td><b>$0.39</b></td>
+    </tr>
+    <tr>
+        <td >Payment Date</td>
+        <td>October 31,2017</td>
+        <td><b>NET PAY</b></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td ><b>$61.39</b></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Approved By </td>
+        <td></td>
+        <td>SIXTY ONE DOLLAR</td>
+    </tr>
+    <tr style="min-height: 50px;"></tr>
+    </tbody>
+</table>
+
